1422ff8a028dd320ed01392ae844820533019709
Tom Prince <tom.prince@hocat.ca>
2022-02-14 10:55:24 -0700

    Set error handling in bash scripts and reorder update steps.

--- flake.nix
+++ flake.nix
@@ -76,6 +76,7 @@
             update-wheel.type = "app";
             update-wheel.program = toString (pkgs.writeScript "update-wheel" ''
               #!/usr/bin/env bash
+              set -xeo pipefail
               ${exports}
               ${pyEnv}/bin/python ${./updater}/crawl_wheel_deps.py
             '');
@@ -84,6 +85,7 @@
             update-sdist.type = "app";
             update-sdist.program = toString (pkgs.writeScript "update-sdist" ''
               #!/usr/bin/env bash
+              set -xeo pipefail
               ${exports}
               ${pyEnv}/bin/python ${./updater}/crawl_sdist_deps.py
             '');
@@ -92,8 +94,7 @@
             job-sdist-wheel.type = "app";
             job-sdist-wheel.program = toString (pkgs.writeScript "job-sdist" ''
               #!/usr/bin/env bash
-              set -e
-              set -x
+              set -xeo pipefail
 
               # update the index to get the newest packages
               indexRevPrev=$(${pkgs.nixFlakes}/bin/nix flake metadata --json | ${pkgs.jq}/bin/jq -e --raw-output '.locks .nodes .pypiIndex .locked .rev')
@@ -104,18 +105,18 @@
                 exit 0
               fi
 
-              # crawl wheel and sdist packages
-              # If CI system has a run time limit, make sure to set MAX_MINUTES_WHEEL and MAX_MINUTES_SDIST
-              # time ratio for wheel/sdist should be around 1/10
-              MAX_MINUTES=''${MAX_MINUTES_WHEEL:-0} ${pkgs.nixFlakes}/bin/nix run .#update-wheel
-              MAX_MINUTES=''${MAX_MINUTES_SDIST:-0} ${pkgs.nixFlakes}/bin/nix run .#update-sdist
-
-              # commit to git
-              echo $(date +%s) > UNIX_TIMESTAMP
               indexHash=$(${pkgs.nixFlakes}/bin/nix flake metadata --json | ${pkgs.jq}/bin/jq -e --raw-output '.locks .nodes .pypiIndex .locked .narHash')
               echo $indexRev > PYPI_FETCHER_COMMIT
               echo $indexHash > PYPI_FETCHER_SHA256
 
+              # crawl wheel and sdist packages
+              # If CI system has a run time limit, make sure to set MAX_MINUTES_WHEEL and MAX_MINUTES_SDIST
+              # time ratio for wheel/sdist should be around 1/10
+              MAX_MINUTES=''${MAX_MINUTES_WHEEL:-0} ${update-wheel.program}
+              MAX_MINUTES=''${MAX_MINUTES_SDIST:-0} ${update-sdist.program}
+
+              # commit to git
+              echo $(date +%s) > UNIX_TIMESTAMP
               git add sdist sdist-errors wheel flake.lock UNIX_TIMESTAMP PYPI_FETCHER_COMMIT PYPI_FETCHER_SHA256
               git pull origin $(git rev-parse --abbrev-ref HEAD)
               git commit -m "$(date) - update sdist + wheel"
